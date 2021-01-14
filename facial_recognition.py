f422c2af94e72b8cb7c38e6c8bd02b6147cc629f
Alec Echevarria <alecech1994@gmail.com>
2021-01-13 16:35:01 -0800

    now uses scripts to build and update databse of known people

--- facial_recognition.py
+++ facial_recognition.py
@@ -25,8 +25,12 @@ bd(cid, secret)
 with open('encodings.json', 'r') as encodings:
     identityMap = json.loads(encodings.read())
     
-print(identityMap)
-
+known_face_names, known_face_encodings, name_music_map = [], [], {}
+for identity in identityMap:
+    known_face_names.append(identity)
+    known_face_encodings.append(np.array(identityMap[identity][0]))
+    name_music_map[identity] = identityMap[identity][1]
+    
 # =============================================================================
 # # Load a sample picture and learn how to recognize it.
 # obama_image = face_recognition.load_image_file("./known_people/Barack Obama.jpg")
@@ -87,80 +91,80 @@ print(identityMap)
 #     "Sophia Arnold": "spotify:track:0GONea6G2XdnHWjNZd6zt3"
 #     }
 # 
-# 
-# 
-# 
-# 
-# #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-# #   2. Only detect faces in every other frame of video.
-# 
-# # Get a reference to webcam #0 (the default one)
-# video_capture = cv2.VideoCapture(0)
-# 
-# # Initialize some variables
-# face_locations = []
-# face_encodings = []
-# face_names = []
-# mapped_names = []
-# songIDs = []
-# process_this_frame = True
-# 
-# # timeout variable can be omitted, if you use specific value in the while condition
-# timeout = 5  # [seconds]
-# timeout_start = time()
-# 
-# while time() < timeout_start + timeout:
-#     # Grab a single frame of video
-#     ret, frame = video_capture.read()
-# 
-#     # Resize frame of video to 1/4 size for faster face recognition processing
-#     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-# 
-#     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-#     rgb_small_frame = small_frame[:, :, ::-1]
-# 
-#     # Only process every other frame of video to save time
-#     if process_this_frame:
-#         # Find all the faces and face encodings in the current frame of video
-#         face_locations = face_recognition.face_locations(rgb_small_frame)
-#         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-# 
-#         face_names = []
-#         for face_encoding in face_encodings:
-#             # See if the face is a match for the known face(s)
-#             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-# 
-#             # Use the known face with the smallest distance to the new face
-#             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-#             best_match_index = np.argmin(face_distances)
-#             if matches[best_match_index]:
-#                 name = known_face_names[best_match_index]
-#             
-#             face_names.append(name)
-#             
-#         for name in face_names:
-#             if name not in mapped_names:
-#                 mapped_names.append(name)
-#                 songIDs.append(name_music_map[name])
-# 
-#     process_this_frame = not process_this_frame
-#     
-#     # displays the video taken from webcam. Not actually necessary to run
-#     # cv2.imshow('Video', frame)
-# 
-#     # Hit 'q' on the keyboard to quit
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-# 
-# # Release handle to the webcam
-# video_capture.release()
-# cv2.destroyAllWindows()
-# 
-# # Run script that plays Spotify based on matched faces
-# if songIDs:
-#     print(songIDs)
-#     sp(songIDs, cid, secret)
-# 
-# 
-# 
 # =============================================================================
+
+
+
+
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+mapped_names = []
+songIDs = []
+process_this_frame = True
+
+# timeout variable can be omitted, if you use specific value in the while condition
+timeout = 5  # [seconds]
+timeout_start = time()
+
+while time() < timeout_start + timeout:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+            # Use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+            
+            face_names.append(name)
+            
+        for name in face_names:
+            if name not in mapped_names:
+                mapped_names.append(name)
+                songIDs.append(name_music_map[name])
+
+    process_this_frame = not process_this_frame
+    
+    # displays the video taken from webcam. Not actually necessary to run
+    # cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
+# Run script that plays Spotify based on matched faces
+if songIDs:
+    print(songIDs)
+    sp(songIDs, cid, secret)
+
+
+
