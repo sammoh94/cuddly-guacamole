f00587461af357a11c79ab85694f8519cbedf40f
Alec Echevarria <alecech1994@gmail.com>
2021-02-01 22:59:53 -0800

    Altered facial recogntion code to allow facial recognition at farther distances

--- facial_recognition.py
+++ facial_recognition.py
@@ -69,16 +69,17 @@ def run_facial_recognition(cid = cid, secret = secret, time_out = 5, face_locati
         ret, frame = video_capture.read()
     
         # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # Note: this is faster, but prevents facial recognition at farther distances
+        # small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
+        rgb_frame = frame[:, :, ::-1]
     
         # Only process every other frame of video to save time
         if process_frame:
             # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_locations = face_recognition.face_locations(rgb_frame)
+            face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
     
             face_names = []
             for face_encoding in face_encodings:
